5d4d7509c9c1488f39d6f9125306ab12f7d6c4cc
HassanRashid <hassanrashid0018@gmail.com>
2025-01-23 11:10:17 +0500

    New Changes Added

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
     $('.slick-slider').slick({
       infinite: true,         // Loop through slides
-      slidesToShow: 3,        // Number of slides to show
+      slidesToShow: 4,        // Number of slides to show
       slidesToScroll: 1,      // Number of slides to scroll
       dots: true,             // Show navigation dots
-      arrows: true,           // Show navigation arrows
-      autoplay: true,         // Enable autoplay
+      arrows: true,      
+      centerPadding: "30px",      // Show navigation arrows
+      autoplay: false,         // Enable autoplay
       autoplaySpeed: 3000,    // Time between slides (in ms)
       responsive: [
         {
-          breakpoint: 768,    // For smaller screens
+          breakpoint: 992,    // For smaller screens
           settings: {
-            slidesToShow: 1   // Show 1 slide on mobile
+            slidesToShow: 2   // Show 1 slide on mobile
           }
         }
       ]
